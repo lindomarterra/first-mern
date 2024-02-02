214bdb45342820a269d2afc9a4655d77c7ec44bc
Terra <terra.lindomar@gmail.com>
2024-02-02 19:59:05 -0300

    class 09

--- src/database/db.js
+++ src/database/db.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const connectDatabase = () => {
-  console.log("wait connecting to the database");
+  console.log("wait connecting to the database")
 
   mongoose
-    .connect(   //mongoose.connect é um codigo asyncrono precisa de javascript para fazer esperar o resposta
+    .connect(  //mongoose.connect é um codigo asyncrono precisa de javascript para fazer esperar o resposta
       "mongodb+srv://terra:mernstackdeveloper@cluster0.gmedg21.mongodb.net/?retryWrites=true&w=majority",
       { useNewUrlParser: true, useUnifiedTopology: true }
     )
     .then(() => console.log("MongoDB Atlas connected!"))  //se a promessa de vem do servidor for ok executará o then
-    .catch((error) => console.log(error));
-};
+    .catch((error) => console.log(error))
+}
 
-module.exports = connectDatabase;
+module.exports = connectDatabase
