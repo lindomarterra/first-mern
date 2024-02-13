7c62ef67dd396e242169be5f219b2e868bc11f15
Terra <terra.lindomar@gmail.com>
2024-02-13 20:41:52 -0300

    restar all  class

--- src/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
- // {Router} é uma função do express por isso pode ser desconstruido usando {}
- import { Router } from "express" 
- const router = Router()
-
- import { login } from '../controllers/auth.controller.js'
-
- router.post("/", login)
-
- export default router
--- src/services/auth.service.js
+++
@@ -1,5 +0,0 @@
-import User from "../models/User.js" //models vai se comunicar com banco de dadas
-
-const loginService = (email) => User.findOne({email: email}) //chaves vai ser um filtro de busca
-
-export { loginService }
